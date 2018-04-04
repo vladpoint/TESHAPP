ee88df5129c5e2cc2a3e0416b2080979e95c3b5a
Vladimir Medina Garcia <vladimir_mg@hotmail.com>
2018-04-04 18:49:19 -0500

    Add project files.

---
+++ TESHAPP/Script.js
@@ -0,0 +1,9 @@
+﻿function fecha() {
+	document.getElementById("fecha").innerText = Date();
+}
+function cuenta() {
+	var x = 3;
+	var y = ++x;
+	var z = x+2;
+	document.getElementById("fecha").innerText= z;
+}
